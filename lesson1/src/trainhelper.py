13cffdd0ac8509898f4e570da2432d0fcc7e3a95
Gabriel Adomnicai <gabesoft@gmail.com>
2017-09-25 08:00:20 -0700

    Plot helper + train helper fixes

--- lesson1/src/trainhelper.py
+++ lesson1/src/trainhelper.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from keras.preprocessing import image
 from time import time
 
@@ -15,16 +17,13 @@ def get_batches(path,
                                    batch_size=batch_size)
 
 
-def finetune_model(vgg, path, batch_size=64):
-    batches = get_batches(path + '/train', batch_size=batch_size)
-    vgg.finetune(batches)
-    vgg.model.optimizer.lr = 0.01
-
-
 def train_model(vgg, data_path, batch_size=64, epochs=3):
     t_batches = get_batches(data_path + '/train', batch_size=batch_size)
     v_batches = get_batches(data_path + '/valid', batch_size=batch_size * 2)
     results_path = data_path + '/results'
+
+    vgg.finetune(t_batches)
+    vgg.model.optimizer.lr = 0.01
 
     for epoch in range(epochs):
         print("Running epoch: %d" % epoch)
@@ -33,3 +32,13 @@ def train_model(vgg, data_path, batch_size=64, epochs=3):
         vgg.model.save_weights(results_path + '/' + weights_file)
 
     print("Completed %d fit operations" % epochs)
+
+
+def test_model(vgg, data_path, batch_size=8):
+    batches = get_batches(data_path,
+                          shuffle=False,
+                          batch_size=batch_size,
+                          class_mode=None)
+    steps = int(np.ceil(batches.samples/batch_size))
+    preds = vgg.model.predict_generator(batches, steps)
+    return batches, preds
