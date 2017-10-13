ccd4475cc5c6ae32e7ace6222db137c17458f8f3
Gabriel Adomnicai <gabesoft@gmail.com>
2017-10-12 23:17:06 -0700

    Statefarm notebook tweaks

--- src/utils/trainhelper.py
+++ src/utils/trainhelper.py
@@ -1,8 +1,11 @@
 import numpy as np
+import os
 
+from keras.models import model_from_json
 from keras.preprocessing import image
 from time import strftime
 from utils.utils import mkdir
+from keras.utils import to_categorical as onehot
 
 
 def get_batches(path,
@@ -16,6 +19,32 @@ def get_batches(path,
                                    class_mode=class_mode,
                                    shuffle=shuffle,
                                    batch_size=batch_size)
+
+
+def get_classes(data_dir):
+    train_dir = os.path.join(data_dir, 'train')
+    valid_dir = os.path.join(data_dir, 'valid')
+    test_dir = os.path.join(data_dir, 'test')
+
+    train_batches = get_batches(train_dir, shuffle=False, batch_size=1)
+    valid_batches = get_batches(valid_dir, shuffle=False, batch_size=1)
+    test_batches = get_batches(test_dir, shuffle=False, batch_size=1)
+
+    train_classes = train_batches.classes
+    valid_classes = valid_batches.classes
+
+    train_labels = onehot(train_classes, train_batches.num_class)
+    valid_labels = onehot(valid_classes, valid_batches.num_class)
+
+    return (
+        train_classes,
+        valid_classes,
+        train_labels,
+        valid_labels,
+        train_batches.filenames,
+        valid_batches.filenames,
+        test_batches.filenames
+    )
 
 
 def train_model(vgg, data_path, batch_size=64, epochs=3):
@@ -46,3 +75,23 @@ def test_model(vgg, data_path, batch_size=8):
     steps = int(np.ceil(batches.samples/batch_size))
     preds = vgg.model.predict_generator(batches, steps)
     return batches, preds
+
+
+def save_model(data_dir, model):
+    json_data = model.to_json()
+    cache_dir = os.path.join(data_dir, 'cache')
+    t = strftime('%Y-%m-%d-%H')
+    architecture_file = 'architecture-%s.json' % t
+    weights_file = 'model-weights-%s.json' % t
+
+    mkdir(cache_dir)
+    open(os.path.join(cache_dir, architecture_file), 'w').write(json_data)
+    model.save_weights(os.path.join(cache_dir, weights_file), overwrite=True)
+
+
+def read_model(data_dir, architecture_file, weights_file):
+    cache_dir = os.path.join(data_dir, 'cache')
+    json_data = open(os.path.join(cache_dir, architecture_file)).read()
+    model = model_from_json(json_data)
+    model.load_weights(os.path.join(cache_dir, weights_file))
+    return model
